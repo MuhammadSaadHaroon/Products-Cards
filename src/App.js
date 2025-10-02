8d3e15ad6c0996e8532e140c30f2a4fb2cdb9a86
MuhammadSaadHaroon <saadharoon6800@gmail.com>
2025-10-02 16:30:43 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Products from "./components/Products/Products";
+import Contact from "./components/Contact/Contact";
+import Footer from './components/Footer/Footer';
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path='/products' element={<Products/>}/>
+        <Route path='/contact-us' element={<Contact/>}/>
+      </Routes>
+      <Footer />
+    </>
+  )
+}
+
+export default App
